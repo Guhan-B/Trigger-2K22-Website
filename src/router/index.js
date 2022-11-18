9e98e541089647d55dc8923ec26ead4a718c0962
Guhan.B <bkguhan2001@gmail.com>
2022-11-19 00:04:30 +0530

    Temporary route patch

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LandingPage from "../pages/Landing";
 import EventPage from "../pages/Event";
